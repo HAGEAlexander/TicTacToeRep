2ce0add909e67d1417be3c7b7d615d34e6a53295
HAGEAlexander <wi23b042@technikum-wien.at>
2024-06-11 14:34:07 +0200

    Create Board.java
    Created a seperate Class for Board and Printboard

---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Board {
+    private String[] board;
+
+    public Board(String[] board) {
+        this.board = board;
+    }
+
+    public void printBoard() {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+        System.out.println("|---|---|---|");
+    }
+}
